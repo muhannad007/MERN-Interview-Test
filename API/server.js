048c2198ca055a2e7218f786f512e6a9f92e54a5
muhannad007 <muhannadafeefe654@gmail.com>
2024-09-14 16:35:19 +0300

    test

---
+++ API/server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const router = require("./routes/Routes");
+require("dotenv").config();
+
+const { PORT, MONGO_PATH, MONGO_USER, MONGO_PASSWORD } = process.env;
+
+const app = express();
+
+const uri = `mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}${MONGO_PATH}`;
+const clientOptions = {
+  serverApi: {
+    version: "1",
+    strict: true,
+    deprecationErrors: true,
+    useNEWUrlParser: true,
+    useUnifiedTopology: true,
+  },
+};
+mongoose
+  .connect(uri, clientOptions)
+  .then((result) => app.listen(PORT))
+  .catch((err) => console.log(err));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+app.use("/api/workouts", router);
